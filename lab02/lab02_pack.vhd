7917d1787ff67c561ebd2677f77444657bf44f1b
ZentetsuKen <as51796957@hotmail.com>
2010-11-30 11:39:18 +0700

    Modify lab02 lab03

--- lab02/lab02_pack.vhd
+++ lab02/lab02_pack.vhd
@@ -11,16 +11,16 @@ package lab02_pack is
 
 -- Declare constants
 
-  constant zero_seg		:std_logic_vector( 7 downto 0 ) := "11111110" ;
-  constant one_seg		:std_logic_vector( 7 downto 0 ) := "01100000" ;
-  constant two_seg		:std_logic_vector( 7 downto 0 ) := "11011010" ;
-  constant three_seg		:std_logic_vector( 7 downto 0 ) := "11110010" ;
-  constant four_seg		:std_logic_vector( 7 downto 0 ) := "01100110" ;
-  constant five_seg		:std_logic_vector( 7 downto 0 ) := "10110110" ;
-  constant six_seg		:std_logic_vector( 7 downto 0 ) := "10111110" ;
-  constant seven_seg		:std_logic_vector( 7 downto 0 ) := "11100000" ;
-  constant eight_seg		:std_logic_vector( 7 downto 0 ) := "11111110" ;
-  constant nine_seg		:std_logic_vector( 7 downto 0 ) := "11110110" ;
-  constant off_seg		:std_logic_vector( 7 downto 0 ) := "00000000" ;
+  constant zero_seg		:std_logic_vector( 7 downto 0 ) := "00000011" ;
+  constant one_seg		:std_logic_vector( 7 downto 0 ) := "10011111" ;
+  constant two_seg		:std_logic_vector( 7 downto 0 ) := "00100101" ;
+  constant three_seg		:std_logic_vector( 7 downto 0 ) := "00001101" ;
+  constant four_seg		:std_logic_vector( 7 downto 0 ) := "10011001" ;
+  constant five_seg		:std_logic_vector( 7 downto 0 ) := "01001001" ;
+  constant six_seg		:std_logic_vector( 7 downto 0 ) := "01000001" ;
+  constant seven_seg		:std_logic_vector( 7 downto 0 ) := "00011111" ;
+  constant eight_seg		:std_logic_vector( 7 downto 0 ) := "00000001" ;
+  constant nine_seg		:std_logic_vector( 7 downto 0 ) := "00001001" ;
+  constant off_seg		:std_logic_vector( 7 downto 0 ) := "11111110" ;
   
 end lab02_pack;
--- lab03/pa.fromNetlist.tcl
+++ lab03/pa.fromNetlist.tcl
@@ -1,11 +1,10 @@
 
-# PlanAhead Launch Script for Post-Synthesis pin planning, created by Project Navigator
+# PlanAhead Launch Script for Post-Synthesis floorplanning, created by Project Navigator
 
-create_project -name lab03 -dir "C:/Documents and Settings/lab03/planAhead_run_2" -part xc3s500efg320-5
+create_project -name lab03 -dir "D:/VHDL/lab03/planAhead_run_1" -part xc3s500efg320-5
 set_property design_mode GateLvl [get_property srcset [current_run -impl]]
-set_property edif_top_file "C:/Documents and Settings/lab03/seg7_decoder.ngc" [ get_property srcset [ current_run ] ]
-add_files -norecurse { {C:/Documents and Settings/lab03} }
-set_param project.pinAheadLayout  yes
+set_property edif_top_file "D:/VHDL/lab03/seg7_decoder.ngc" [ get_property srcset [ current_run ] ]
+add_files -norecurse { {D:/VHDL/lab03} }
 set_param project.paUcfFile  "seg7_constrain.ucf"
 add_files "seg7_constrain.ucf" -fileset [get_property constrset [current_run]]
 open_netlist_design
